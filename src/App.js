9e1d3012fa263522782993e2bb605494fead154b
Dieferson de Oliveira <diiieferson@gmail.com>
2021-09-14 17:26:09 -0300

    atualizado 14-09-2021

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useState } from 'react'
+import { map } from 'async';
 
 function App() {
+const [usuario, setUsuario] = useState('')
+const [erro, setErro] = useState(false)
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => { 
+      
+    console.log(response.data)
+    const picture = response.data
+      const urlPicture = ''
+      localStorage.setItem('foto',map((avatar_url) => {
+        urlPicture.push(picture.avatar_url)
+      }))
+
+      }).catch(err => {
+        setErro(true)
+      })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <input className="input"  placeholder="Usuário" value={usuario} type="usuario" onChange={e => setUsuario(e.target.value)}></input>
+        <button type="button"  >Pesquisar</button>
+      </div>
+      {erro ? <h1>Ocorreu um erro</h1> : 'sem erro'}
+    </>
   );
 }
 
